d963829e98b856608e149fe77a60399497807306
yossi yedid <yedid1324@gmail.com>
2018-08-18 12:10:15 +0000

    add mlab db

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var commentRoutes   = require("./routes/comments"),
     indexRoutes     = require("./routes/index");
 
 
-
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });
+mongoose.connect("mongodb://yossi:yossidb1@ds225442.mlab.com:25442/yossi_yelp_camp",{ useNewUrlParser: true }); //blab cloud db
+//mongoose.connect("mongodb://localhost:27017/yelp_camp",{ useNewUrlParser: true });    //local db
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+ "/public"));
